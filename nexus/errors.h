4450dac6c7073a9debf25f9cc98c627c0fec259a
Numerio <b.vitruvio@gmail.com>
2026-01-30 15:45:46 +0100

    Update errors

--- nexus/errors.h
+++ nexus/errors.h
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (C) 2022-2025 Dario Casalinuovo
+ * Copyright (C) 2022-2026 Dario Casalinuovo
  */
 
 #ifndef _NEXUS_ERRORS_H
@@ -9,6 +9,9 @@
 #define B_GENERAL_ERROR_BASE		INT_MIN
 #define B_OS_ERROR_BASE				(B_GENERAL_ERROR_BASE + 0x2000)
 
+#define B_TIMED_OUT					(B_GENERAL_ERROR_BASE + 11)
+
+#define B_BAD_SEM_ID				(B_OS_ERROR_BASE + 1)
 #define B_NO_MORE_SEMS				(B_OS_ERROR_BASE + 2)
 #define B_BAD_THREAD_ID				(B_OS_ERROR_BASE + 3)
 #define B_BAD_PORT_ID				(B_OS_ERROR_BASE + 8)
@@ -17,16 +20,12 @@
 #define B_BAD_TEAM_ID				-ESRCH
 #define B_BAD_VALUE					-EINVAL
 #define B_NO_MEMORY					-ENOMEM
-
+#define B_ENTRY_NOT_FOUND			-ENOENT
+#define B_WOULD_BLOCK				-EAGAIN
+#define B_INTERRUPTED				-EINTR
 
 #define B_ERROR						(-1)
 #define B_OK						((int)0)
 #define B_NO_ERROR					((int)0)
-
-#define B_TIMED_OUT					(B_GENERAL_ERROR_BASE + 11)
-#define B_WOULD_BLOCK				-EAGAIN
-#define B_INTERRUPTED				-EINTR
-
-#define B_BAD_SEM_ID				(B_OS_ERROR_BASE + 1)
 
 #endif
