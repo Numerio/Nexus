eac0b008d1bb770c6c962187b451d6cf2673c317
Numerio <b.vitruvio@gmail.com>
2026-03-23 21:34:40 +0100

    errors: fix mappings to match v\os

--- nexus/errors.h
+++ nexus/errors.h
@@ -9,13 +9,13 @@
 #define B_GENERAL_ERROR_BASE		INT_MIN
 #define B_OS_ERROR_BASE				(B_GENERAL_ERROR_BASE + 0x2000)
 
-#define B_TIMED_OUT					(B_GENERAL_ERROR_BASE + 11)
+#define B_TIMED_OUT					-ETIMEDOUT
 
 #define B_BAD_SEM_ID				(B_OS_ERROR_BASE + 1)
-#define B_NO_MORE_SEMS				(B_OS_ERROR_BASE + 2)
+#define B_NO_MORE_SEMS				-ENOLCK
 #define B_BAD_THREAD_ID				(B_OS_ERROR_BASE + 3)
 #define B_BAD_PORT_ID				(B_OS_ERROR_BASE + 8)
-#define B_NO_MORE_PORTS				(B_OS_ERROR_BASE + 9)
+#define B_NO_MORE_PORTS				-ENFILE
 
 #define B_BAD_TEAM_ID				-ESRCH
 #define B_BAD_VALUE					-EINVAL
@@ -23,6 +23,8 @@
 #define B_ENTRY_NOT_FOUND			-ENOENT
 #define B_WOULD_BLOCK				-EAGAIN
 #define B_INTERRUPTED				-EINTR
+
+#define B_NOT_ALLOWED				(B_GENERAL_ERROR_BASE + 16)
 
 #define B_ERROR						(-1)
 #define B_OK						((int)0)
