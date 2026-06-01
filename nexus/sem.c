60643074ead321f07510f8bc062d564240757b4e
Numerio <b.vitruvio@gmail.com>
2026-06-01 14:44:52 +0200

    sem: Fix race condition + refactor

--- nexus/sem.c
+++ nexus/sem.c
@@ -20,12 +20,9 @@
 static DEFINE_IDR(nexus_sem_idr);
 static DEFINE_SPINLOCK(sem_idr_lock);
 
-/* Wake all waiters with an error status (sem deleted or team exit). */
 static void wake_all_waiters_error(struct nexus_sem *sem, status_t err)
 {
 	struct nexus_sem_waiter *w, *tmp;
-
-	/* sem->lock must be held by caller */
 	list_for_each_entry_safe(w, tmp, &sem->waiters, list) {
 		w->status = err;
 		w->woken = true;
@@ -109,14 +106,14 @@ static int nexus_delete_sem(sem_id id)
 }
 
 static int nexus_acquire_sem(sem_id id, int32_t count, uint32_t flags,
-			     bigtime_t timeout)
+	bigtime_t timeout)
 {
 	struct nexus_sem *sem;
 	struct nexus_sem_waiter waiter;
 	unsigned long iflags;
 	int ret = B_OK;
 	bool has_deadline = false;
-	u64 deadline_ns = 0;
+	uint64_t deadline_ns = 0;
 
 	if (count <= 0)
 		return B_BAD_VALUE;
@@ -125,6 +122,8 @@ static int nexus_acquire_sem(sem_id id, int32_t count, uint32_t flags,
 	sem = idr_find(&nexus_sem_idr, id);
 	if (!sem) {
 		spin_unlock_irqrestore(&sem_idr_lock, iflags);
+		pr_err("nexus: acquire_sem: id=%d not in IDR (tgid=%d pid=%d)\n",
+		       id, current->tgid, current->pid);
 		return B_BAD_SEM_ID;
 	}
 	nexus_sem_get(sem);
@@ -133,6 +132,8 @@ static int nexus_acquire_sem(sem_id id, int32_t count, uint32_t flags,
 	spin_lock_irqsave(&sem->lock, iflags);
 
 	if (sem->deleted) {
+		pr_err("nexus: acquire_sem: id=%d is deleted (tgid=%d pid=%d)\n",
+		       id, current->tgid, current->pid);
 		ret = B_BAD_SEM_ID;
 		goto out_unlock;
 	}
@@ -152,7 +153,7 @@ static int nexus_acquire_sem(sem_id id, int32_t count, uint32_t flags,
 
 	if (flags & B_ABSOLUTE_TIMEOUT) {
 		has_deadline = true;
-		deadline_ns = (u64)timeout * 1000ULL;
+		deadline_ns = (uint64_t)timeout * 1000ULL;
 		if (ktime_get_ns() >= deadline_ns) {
 			ret = B_TIMED_OUT;
 			goto out_unlock;
@@ -160,22 +161,24 @@ static int nexus_acquire_sem(sem_id id, int32_t count, uint32_t flags,
 	} else if ((flags & B_RELATIVE_TIMEOUT) && timeout > 0 &&
 			timeout != B_INFINITE_TIMEOUT) {
 		has_deadline = true;
-		deadline_ns = ktime_get_ns() + (u64)timeout * 1000ULL;
+		deadline_ns = ktime_get_ns() + (uint64_t)timeout * 1000ULL;
 	}
 
-	/* Enqueue waiter */
 	waiter.task = current;
 	waiter.count = count;
 	waiter.status = B_OK;
 	waiter.woken = false;
 	list_add_tail(&waiter.list, &sem->waiters);
 
+	int wait_state = (flags & B_CAN_INTERRUPT)
+		? TASK_INTERRUPTIBLE : TASK_KILLABLE;
+
 	for (;;) {
-		set_current_state(TASK_INTERRUPTIBLE);
+		set_current_state(wait_state);
 		spin_unlock_irqrestore(&sem->lock, iflags);
 
 		if (has_deadline) {
-			ktime_t deadline = ns_to_ktime((s64)deadline_ns);
+			ktime_t deadline = ns_to_ktime((int64_t)deadline_ns);
 			schedule_hrtimeout_range(&deadline, 0, HRTIMER_MODE_ABS);
 		} else {
 			schedule();
@@ -243,9 +246,6 @@ static int nexus_release_sem(sem_id id, int32_t count, uint32_t flags)
 	}
 
 	if (flags & B_RELEASE_ALL) {
-		/* Wake every waiter regardless of count. Haiku passes a
-		 * negative `count` as the unblock status so callers can
-		 * broadcast errors (e.g. B_INTERRUPTED on teardown). */
 		status_t unblock_status = (count < 0) ? count : B_OK;
 
 		sem->count = 0;
@@ -260,8 +260,6 @@ static int nexus_release_sem(sem_id id, int32_t count, uint32_t flags)
 		int32_t initial_count = sem->count;
 		sem->count += count;
 
-		/* FIFO; stop at the first waiter we can't satisfy so a
-		 * large-count acquirer isn't starved by smaller ones behind it. */
 		list_for_each_entry_safe(w, tmp, &sem->waiters, list) {
 			if (sem->count < w->count)
 				break;
@@ -273,8 +271,6 @@ static int nexus_release_sem(sem_id id, int32_t count, uint32_t flags)
 			wake_up_process(w->task);
 		}
 
-		/* B_RELEASE_IF_WAITING_ONLY: discard any portion of `count`
-		 * that wasn't consumed by waking waiters. */
 		if ((flags & B_RELEASE_IF_WAITING_ONLY) && sem->count > initial_count)
 			sem->count = initial_count;
 	}
@@ -360,19 +356,28 @@ void nexus_sem_team_exit(pid_t team)
 	struct nexus_sem *sem;
 	unsigned long flags;
 	int id;
+	LIST_HEAD(dead);
 
 	spin_lock_irqsave(&sem_idr_lock, flags);
 	idr_for_each_entry(&nexus_sem_idr, sem, id) {
 		if (sem->owner != (team_id)team)
 			continue;
+		idr_remove(&nexus_sem_idr, id);
 		spin_lock(&sem->lock);
 		if (!sem->deleted) {
 			sem->deleted = true;
 			wake_all_waiters_error(sem, B_BAD_SEM_ID);
 		}
 		spin_unlock(&sem->lock);
+		list_add(&sem->waiters, &dead);
 	}
 	spin_unlock_irqrestore(&sem_idr_lock, flags);
+
+	while (!list_empty(&dead)) {
+		sem = list_first_entry(&dead, struct nexus_sem, waiters);
+		list_del_init(&sem->waiters);
+		nexus_sem_put(sem);
+	}
 }
 
 int nexus_sem_init(void)
@@ -399,26 +404,22 @@ void nexus_sem_exit(void)
 	spin_unlock(&sem_idr_lock);
 }
 
-/* ioctl wrappers */
-
 static long nexus_sem_ioctl_create(unsigned long arg)
 {
 	struct nexus_sem_create ex;
 	sem_id id;
-	long ret;
 
 	if (copy_from_user(&ex, (void __user *)arg, sizeof(ex)))
-		return B_BAD_VALUE;
+		return -EFAULT;
 
-	ret = nexus_create_sem(ex.count, ex.name, &id);
-	if (ret != B_OK)
-		return ret;
+	ex.ret = nexus_create_sem(ex.count, ex.name, &id);
+	if (ex.ret == B_OK)
+		ex.id = id;
 
-	ex.id = id;
 	if (copy_to_user((void __user *)arg, &ex, sizeof(ex)))
-		return B_BAD_VALUE;
+		return -EFAULT;
 
-	return B_OK;
+	return 0;
 }
 
 static long nexus_sem_ioctl_delete(unsigned long arg)
@@ -426,9 +427,14 @@ static long nexus_sem_ioctl_delete(unsigned long arg)
 	struct nexus_sem_delete_req ex;
 
 	if (copy_from_user(&ex, (void __user *)arg, sizeof(ex)))
-		return B_BAD_VALUE;
+		return -EFAULT;
 
-	return nexus_delete_sem(ex.id);
+	ex.ret = nexus_delete_sem(ex.id);
+
+	if (copy_to_user((void __user *)arg, &ex, sizeof(ex)))
+		return -EFAULT;
+
+	return 0;
 }
 
 static long nexus_sem_ioctl_acquire(unsigned long arg)
@@ -436,9 +442,14 @@ static long nexus_sem_ioctl_acquire(unsigned long arg)
 	struct nexus_sem_op ex;
 
 	if (copy_from_user(&ex, (void __user *)arg, sizeof(ex)))
-		return B_BAD_VALUE;
+		return -EFAULT;
 
-	return nexus_acquire_sem(ex.id, ex.count, ex.flags, ex.timeout);
+	ex.ret = nexus_acquire_sem(ex.id, ex.count, ex.flags, ex.timeout);
+
+	if (copy_to_user((void __user *)arg, &ex, sizeof(ex)))
+		return -EFAULT;
+
+	return 0;
 }
 
 static long nexus_sem_ioctl_release(unsigned long arg)
@@ -446,63 +457,59 @@ static long nexus_sem_ioctl_release(unsigned long arg)
 	struct nexus_sem_op ex;
 
 	if (copy_from_user(&ex, (void __user *)arg, sizeof(ex)))
-		return B_BAD_VALUE;
+		return -EFAULT;
 
-	return nexus_release_sem(ex.id, ex.count, ex.flags);
+	ex.ret = nexus_release_sem(ex.id, ex.count, ex.flags);
+
+	if (copy_to_user((void __user *)arg, &ex, sizeof(ex)))
+		return -EFAULT;
+
+	return 0;
 }
 
 static long nexus_sem_ioctl_count(unsigned long arg)
 {
 	struct nexus_sem_count_req ex;
-	long ret;
 
 	if (copy_from_user(&ex, (void __user *)arg, sizeof(ex)))
-		return B_BAD_VALUE;
+		return -EFAULT;
 
-	ret = nexus_get_sem_count(ex.id, &ex.count);
-	if (ret != B_OK)
-		return ret;
+	ex.ret = nexus_get_sem_count(ex.id, &ex.count);
 
 	if (copy_to_user((void __user *)arg, &ex, sizeof(ex)))
-		return B_BAD_VALUE;
+		return -EFAULT;
 
-	return B_OK;
+	return 0;
 }
 
 static long nexus_sem_ioctl_info(unsigned long arg)
 {
 	struct nexus_sem_info_req ex;
-	long ret;
 
 	if (copy_from_user(&ex, (void __user *)arg, sizeof(ex)))
-		return B_BAD_VALUE;
+		return -EFAULT;
 
-	ret = nexus_get_sem_info(ex.id, &ex.info);
-	if (ret != B_OK)
-		return ret;
+	ex.ret = nexus_get_sem_info(ex.id, &ex.info);
 
 	if (copy_to_user((void __user *)arg, &ex, sizeof(ex)))
-		return B_BAD_VALUE;
+		return -EFAULT;
 
-	return B_OK;
+	return 0;
 }
 
 static long nexus_sem_ioctl_next_info(unsigned long arg)
 {
 	struct nexus_sem_next_info ex;
-	long ret;
 
 	if (copy_from_user(&ex, (void __user *)arg, sizeof(ex)))
-		return B_BAD_VALUE;
+		return -EFAULT;
 
-	ret = nexus_get_next_sem_info(ex.team, ex.cookie, &ex);
-	if (ret != B_OK)
-		return ret;
+	ex.ret = nexus_get_next_sem_info(ex.team, ex.cookie, &ex);
 
 	if (copy_to_user((void __user *)arg, &ex, sizeof(ex)))
-		return B_BAD_VALUE;
+		return -EFAULT;
 
-	return B_OK;
+	return 0;
 }
 
 long nexus_sem_ioctl(unsigned int cmd, unsigned long arg)
