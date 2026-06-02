4d20995fc777ef1a476a820eaa282701a1ff0070
Numerio <b.vitruvio@gmail.com>
2026-06-02 14:39:06 +0200

    query: Update stubs to allow building

--- nexus/query.c
+++ nexus/query.c
@@ -1,5 +1,17 @@
 // SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2026 Dario Casalinuovo
+ */
+
 #include <linux/kernel.h>
+#include <linux/errno.h>
 #include "query.h"
+
 int nexus_query_init(void) { pr_info("nexus_query: initialized (stub)\n"); return 0; }
 void nexus_query_exit(void) {}
+
+long nexus_query_ioctl_open(unsigned long arg)
+{
+	(void)arg;
+	return -ENOSYS;
+}
--- nexus/query.h
+++ nexus/query.h
@@ -1,5 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2026 Dario Casalinuovo
+ */
+
 #ifndef _NEXUS_QUERY_H
 #define _NEXUS_QUERY_H
+
 int  nexus_query_init(void);
 void nexus_query_exit(void);
-#endif
+long nexus_query_ioctl_open(unsigned long arg);
+
+#endif /* _NEXUS_QUERY_H */
